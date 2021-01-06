ca7f9add4a4dae33661856cd1ec369ba56918e51
Ninjeng <you@example.com>
2021-01-06 16:58:38 +0545

    Project completed

---
+++ android/app/src/main/kotlin/com/ninzen/grp_chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ninzen.grp_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'screeens/welcome_screen.dart';
+import 'screeens/login_screen.dart';
+import 'screeens/registration_screen.dart';
+import 'screeens/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(ChatApp());
+}
+
+class ChatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.light(),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+      },
+    );
+  }
+}
